b9667e6b2b772d431126723fc6bc0009805fa447
Vedagorich <yas.taras@mail.ru>
2022-08-09 13:54:23 +0300

    добавил графическое поле

--- IMPORTS/mylibrary/function.py
+++ IMPORTS/mylibrary/function.py
@@ -1,40 +1,43 @@
 from pytube import YouTube
-import tkinter as tk
+from tkinter import *
 
+a,b = 0,1
 
-class Load_App(tk.Tk):
+class Load_App(Tk):
 
     def __init__(self):
         super().__init__()
-        self.load_v = tk.Entry(self)
-        self.login_btn = tk.Button(self, text="Скачать",
-                                   command=self.print_login)
-        self.clear_btn = tk.Button(self, text="Очистить",
-                                   command=self.clear_form)
+        self.title("Загрузчик")
+        self.geometry("500x500")
+        self.msg = Message(self, text=a)
+
+
+        self.load_v = Entry(self, width=100)
+        self.login_btn = Button(self, text="Скачать",
+                                command=self.print_login)
+        self.clear_btn = Button(self, text="Очистить",
+                                command=self.clear_form)
         self.load_v.pack()
-        self.login_btn.pack(fill=tk.BOTH)
-        self.clear_btn.pack(fill=tk.BOTH)
+        self.login_btn.pack(fill=BOTH)
+        self.clear_btn.pack(fill=BOTH)
+        self.msg.pack()
+
 
     def print_login(self):
-        link = ("Логин: {}".format(self.load_v.get()))
+        global a,b
+        link = ("ссылка: {}".format(self.load_v.get()))
         yt = YouTube(link)
+        a = "загружаю"
         ys = yt.streams.get_highest_resolution()
-        print("Еще немножечко...")
         ys.download("Desktop\ютубчик")
-        print("Лови видюху. ГОТОВО!!!!")
+        b = "Готово!"
 
     def clear_form(self):
-        self.load_v.delete(0, tk.END)
+        self.load_v.delete(0, END)
         self.load_v.focus_set()
 
 
 if __name__ == "__main__":
     app = Load_App()
-    app.mainloop()
 
-    link = input("Введите ссылку с ютуба:  ")
-    yt = YouTube(link)
-    ys = yt.streams.get_highest_resolution()
-    print("Еще немножечко...")
-    ys.download("Desktop\ютубчик")
-    print("Лови видюху. ГОТОВО!!!!")
+    app.mainloop()
